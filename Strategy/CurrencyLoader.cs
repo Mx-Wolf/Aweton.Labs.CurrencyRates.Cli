6284d72ee26497b4f57b7d1cc556c9da352d01c9
Maxim Volkov <mx_ruz_net@hotmail.com>
2022-03-31 12:43:44 +0300

    gets ahead of time. will it be ok to have only WithScope as it is

--- Strategy/CurrencyLoader.cs
+++ Strategy/CurrencyLoader.cs
@@ -1,8 +1,16 @@
 using Aweton.Labs.CurrencyRates.Cli.Models;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace Aweton.Labs.CurrencyRates.Cli.Strategy;
 internal class CurrencyLoader
 {  
+  private readonly IServiceProvider m_Services;
+
+  public CurrencyLoader(IServiceProvider services)
+  {
+    m_Services = services;
+  }
+
   public async Task Run()
   {
     await Register(await Fetch(await Initialize()));
@@ -38,5 +46,11 @@ internal class CurrencyLoader
   private Task<DateTime> GetLastKnownFetchDate()
   {
     throw new NotImplementedException();
+  }
+
+  private async Task<TResult> WithScope<TWorker, TResult>(Func<TWorker, Task<TResult>> callback) where TWorker :notnull
+  {
+    using var scope = m_Services.CreateScope();
+    return await callback(scope.ServiceProvider.GetRequiredService<TWorker>());
   }
 }
