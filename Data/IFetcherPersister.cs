2f4b6b45eac1bfc85b04e07332c79c1e6a1b51da
Maxim Volkov <mx_ruz_net@hotmail.com>
2022-03-31 13:16:59 +0300

    again jumping ahead on time

--- Data/IFetcherPersister.cs
+++ Data/IFetcherPersister.cs
@@ -1,7 +1,29 @@
 using Aweton.Labs.CurrencyRates.Cli.Models;
+using Microsoft.Extensions.Options;
 
 namespace Aweton.Labs.CurrencyRates.Cli.Data;
 
-public interface IFetcherPersister{
-  Task<IReadOnlyList<CurrencyRate>> Load(DateTime after, IReadOnlyDictionary<int,string> currencyNameToIdMap);
+public interface IFetchWorker
+{
+  Task<IReadOnlyList<CurrencyRate>> Load(DateTime after, IReadOnlyDictionary<int, string> currencyNameToIdMap);
+}
+public class FetchWorkerSettins
+{
+  public string Url { get; set; } = null!;
+}
+internal class FetchWorker : IFetchWorker
+{
+  private readonly string m_Url;
+  private readonly HttpClient m_HttpClient;
+
+  public FetchWorker(IOptions<FetchWorkerSettins> settings, HttpClient httpClient)
+  {
+    m_Url = settings.Value.Url;
+    m_HttpClient = httpClient;
+  }
+
+  public Task<IReadOnlyList<CurrencyRate>> Load(DateTime after, IReadOnlyDictionary<int, string> currencyNameToIdMap)
+  {
+    throw new NotImplementedException();
+  }
 }
